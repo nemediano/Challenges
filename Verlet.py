4d74c2cf234f6b6d520ec85436c5051a241d941e
Christopher Hagmann <cdhagmann@gmail.com>
2014-10-31 18:41:47 -0400

    First finished versionof Verlet.py

--- Verlet.py
+++ Verlet.py
@@ -7,9 +7,23 @@ Created on Fri Oct 31 18:15:02 2014
 
 import numpy as np
 
-def verlet(F, m, x0, v0, delta_t=.01, t_max=5):
-    x = {0: x0}    
-    x[1] = x[0] + v0 * delta_1
-    for i in xrange(2, int(t_max / delta_T)):
-        x[i] = 2 * x[i] - x[i-1] + F(delta_t * i) / float(m) * 
+def verlet(F, m, x0, v0, Dt=.01, t_max=5):
+    '''
+    Apply Verlet Integration
+    '''
+    nsteps = tmax / Dt
+    
+    x = np.zeros( (nsteps + 1) )
+    v = np.zeros( (nsteps + 1) )
+    t = np.zeros( (nsteps + 1) )
+    
+    x[0] = x0
+    v[0] = v0
+    t[0] = 0.0
+    
+    x[1] = x[0] + v[0] * Dt + 0.5 * Dt ** 2 * F(x[0]) / m
+    for i in xrange(1, nsteps):
+        x[i+1] = 2 * x[i] - x[i-1] + F(x[i]) * Dt ** 2.0 / float(m)
+        
+    return x
     
