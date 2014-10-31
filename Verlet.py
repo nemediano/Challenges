cf11564ad730feb64463905fa402d4631f985e92
Nicolas Guarin-Zapata <nguarin@sslab05.cs.purdue.edu>
2014-10-31 18:53:53 -0400

    Some minor tweaks to the function

--- Verlet.py
+++ Verlet.py
@@ -6,6 +6,12 @@ Created on Fri Oct 31 18:15:02 2014
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def spring(x):
+    k = 1.0
+    return -k*x
 
 def verlet(F, m, x0, v0, Dt=.01, t_max=5):
     '''
@@ -21,9 +27,18 @@ def verlet(F, m, x0, v0, Dt=.01, t_max=5):
     v[0] = v0
     t[0] = 0.0
     
-    x[1] = x[0] + v[0] * Dt + 0.5 * Dt ** 2 * F(x[0]) / m
-    for i in xrange(1, nsteps):
-        x[i+1] = 2 * x[i] - x[i-1] + F(x[i]) * Dt ** 2.0 / float(m)
+    x[1] = x[0] + v[0] * Dt
+    t[1] = Dt
+    for i in xrange(2, nsteps):
+            x[i+1] = 2 * x[i] - x[i-1] + F(x[i]) * Dt ** 2.0 / float(m)
+            t[i+1] = t[i] + Dt
         
-    return x
+    return t, x
+
+if __name__ == "__main__":
     
+    t, x = verlet(spring, 1.0, 1.0, 0.0, Dt=.01, t_max=5)
+
+    plt.plot(t,x)
+    plt.xlabel(r"$t$", size=18)
+    plt.ylabel(r"$x$", size=18)
